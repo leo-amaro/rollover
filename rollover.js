ec0da3a4949974f4b6b8467300d70e0cb07de3ef
leo-amaro-conestoga <lamaro3722@conestogac.on.ca>
2023-10-15 13:09:31 -0400

    final

--- rollover.js
+++ rollover.js
@@ -1,21 +1,50 @@
-"use strict";
-
 document.addEventListener("DOMContentLoaded", () => {
-    const images = document.querySelectorAll("#image_rollovers img");
+  const images = document.querySelectorAll("#image_rollovers img");
 
-    // process each img tag
-    for (let image of images) {
-        const oldURL = image.src;
-        const newURL = image.id;
+  // Array to store the original image URLs
+  const originalImageURLs = [];
 
-        // preload rollover image
+  // Preload the rollover images
+  images.forEach((image) => {
+    const oldURL = image.src;
+    const newURL = image.id;
 
-        // set up event handlers for hovering an image
-        image.addEventListener("mouseover", () => {
-            image.src = newURL;
-        });
-        image.addEventListener("mouseout", () => {
-            image.src = oldURL;
-        });
-    }
+    // Store the original image URLs
+    originalImageURLs.push(oldURL);
+
+    // Preload rollover image
+    const rolloverImage = new Image();
+    rolloverImage.src = newURL;
+
+    // set up event handlers for hovering an image
+    image.addEventListener("mouseover", () => {
+      image.src = newURL;
+    });
+
+    image.addEventListener("mouseout", () => {
+      image.src = oldURL;
+    });
+  });
+
+  // Function to display rollover images
+  function displayRolloverImages() {
+    images.forEach((image, index) => {
+      const newURL = image.id;
+      image.src = newURL;
+    });
+  }
+
+  // Function to display original images
+  function displayOriginalImages() {
+    images.forEach((image, index) => {
+      const oldURL = originalImageURLs[index];
+      image.src = oldURL;
+    });
+  }
+
+  // Add a timer to display rollover images one second after the page loads
+  setTimeout(displayRolloverImages, 1000);
+
+  // Add another timer to display original images two seconds after the page loads
+  setTimeout(displayOriginalImages, 2000);
 });
